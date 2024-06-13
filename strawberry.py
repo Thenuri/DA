018d398ea7312ba04033d3098a654eb7061d502d
Thenuri <cb010529@students.apiit.lk>
2024-06-13 21:17:53 +0530

    new

--- strawberry.py
+++ strawberry.py
@@ -18,16 +18,16 @@ app_mode = st.sidebar.selectbox("Select Page",["Home","About","Disease Recogniti
 # st.set_page_config(page_title="Strawberry Disease Detection", page_icon="🍓", layout="wide")
 #Main Page
 if(app_mode=="Home"):
-    st.header("PLANT DISEASE RECOGNITION SYSTEM")
+    st.header("STRAWBERRY DISEASE RECOGNITION SYSTEM")
     image_path = "Strawberry Disease Data/flowers-fruits-garden-strawberry-plant-species.webp"
     st.image(image_path,use_column_width=True)
     st.markdown("""
-    Welcome to the Plant Disease Recognition System! 🌿🔍
+    Welcome to the Strawberry Disease Recognition System! 🌿🔍
     
     Our mission is to help in identifying plant diseases efficiently. Upload an image of a plant, and our system will analyze it to detect any signs of diseases. Together, let's protect our crops and ensure a healthier harvest!
 
     ### How It Works
-    1. **Upload Image:** Go to the **Disease Recognition** page and upload an image of a plant with suspected diseases.
+    1. **Upload Image:** Go to the **Disease Recognition** page and upload an image of a strawberry with suspected diseases.
     2. **Analysis:** Our system will process the image using advanced algorithms to identify potential diseases.
     3. **Results:** View the results and recommendations for further action.
 
@@ -48,8 +48,8 @@ elif(app_mode=="About"):
     st.header("About")
     st.markdown("""
                 #### About Dataset
-                This dataset is recreated using offline augmentation from the original dataset.The original dataset can be found on this github repo.
-                This dataset consists of about 87K rgb images of healthy and diseased crop leaves which is categorized into 38 different classes.The total dataset is divided into 80/20 ratio of training and validation set preserving the directory structure.
+                This dataset is recreated using from the original dataset.The original dataset can be found on kaggle.
+                This dataset consists of about 2500 rgb images of seven diseased fruit leaves, flowers which is categorized into 38 different classes.The total dataset is divided into 80/20 ratio of training and validation set preserving the directory structure.
                 A new directory containing 33 test images is created later for prediction purpose.
                 #### Content
                 1. train (70295 images)
